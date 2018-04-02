e9fb074867b015729815b7527d4a09a3799a3c28
zeke123 <zekezhoujian@163.com>
2018-04-02 13:50:21 +0800

    周建 提交代码

--- mutipro.py
+++ mutipro.py
@@ -14,3 +14,43 @@ if pid == 0:
     print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
 else:
     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+
+print('--------------------1')
+
+#由于Python是跨平台的，自然也应该提供一个跨平台的多进程支持。multiprocessing模块就是跨平台版本的多进程模块。
+#multiprocessing模块提供了一个Process类来代表一个进程对象，下面的例子演示了启动一个子进程并等待其结束：
+from multiprocessing import Process
+import os
+def run_proc(name):
+    print('Run child process %s(%s)...'% (name,os.getpid()))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
+print('--------------------2')
+# Pool 如果要启动大量的子进程，可以用进程池的方式批量创建子进程：
+from multiprocessing import Pool
+import os,time,random
+def long_time_task(name):
+    print('Run task %s (%s)...'% (name,os.getpgid()))
+    start = time.time()
+    time.sleep(random.random()*3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name,(end - start)))
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
+
+
+
